3d8e729cc87bf2d07f9a48b8b1e08f48bf732ef9
Joshua Miller <jsmiller@uchicago.edu>
2016-02-15 17:25:03 -0600

    wip

--- src/app.rs
+++ src/app.rs
@@ -17,19 +17,20 @@ impl Game {
 
     pub fn new() -> Game {
 
-        // let terrain = Mesh::new_terrain()
-        //     .translate([0.0, 0.0, 0.0]);
+        let terrain = Mesh::new_terrain()
+            .translate([0.0, 0.0, 0.0]);
 
-        let mesh1 = Mesh::new()
-            .face(Face::new([100.0, 0.0, 0.0],
-                            [  0.0, 0.0, 0.0],
-                            [  0.0, 100.0, 0.0])
-                  .color([1.0, 1.0, 1.0, 0.4]))
-            .position([0.0, 0.0, 0.0])
-            .wireframe(false);
+        // let mesh1 = Mesh::new()
+        //     .face(Face::new([ 100.0, 0.0, 0.0],
+        //                     [-100.0, 0.0, 0.0],
+        //                     [  0.0, 100.0, 0.0])
+        //           .color([1.0, 1.0, 1.0, 0.4]))
+        //     .position([0.0, 0.0, 0.0])
+        //     .wireframe(false);
 
         let world = World::new()
-            .object(WorldObject::new().mesh(mesh1));
+            // .object(WorldObject::new().mesh(mesh1));
+            .object(WorldObject::new().mesh(terrain));
 
         Game { world: world }
 
